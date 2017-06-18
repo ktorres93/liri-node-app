5735b3f8c5f33aff672e05979757cdce77009fad
Kevin <ktorres93@knights.ucf.edu>
2017-06-17 23:30:57 -0400

    omdb call complete, added comments and psuedocoding for other functions.

--- liri.js
+++ liri.js
@@ -8,11 +8,40 @@ var nodeArgs = process.argv;
 var request = require("request");
 
 
+//calls last 20 tweets
+// function lastTweets() {
+//
+// }
+
+//does whatever the random.txt file says
+// function runRandomTxt() {
+//
+// }
+//grabs Artist(s), The song's name , A preview link of the song from Spotify, The album that the song is from
+function getSong() {
+    var userinput= "";
+    for (var i = 3; i < nodeArgs.length; i++) {
+        userinput = userinput + " " + nodeArgs[i];
+    }
+    Spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
+    });
+
+}
+//gets data on movies from node prompt
 function getMovie() {
-
-
-//var omdbInput = process.argv[2];
-request("http://www.omdbapi.com/?t=john+wick&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+    //sets user input to empty string
+ var userinput= "";
+    // starting from index 3 of node args loops through args and adds them to the userinput variable
+    for (var i = 3; i < nodeArgs.length; i++) {
+        userinput = userinput + "" + nodeArgs[i];
+    }
+//makes request to omdbapi
+request("http://www.omdbapi.com/?t=" + userinput + "=&plot=short&apikey=40e9cece", function(error, response, body) {
     if (!error && response.statusCode === 200) {
 
         console.log("The movie's Title is: " + JSON.parse(body).Title);
@@ -22,21 +51,23 @@ request("http://www.omdbapi.com/?t=john+wick&y=&plot=short&apikey=40e9cece", fun
         console.log("The movie's language is: " + JSON.parse(body).Language);
         console.log("The movie's plot is: " + JSON.parse(body).Plot);
         console.log("The movie's actors are: " + JSON.parse(body).Actors);
-        //console.log("The movie's Rottentomatos site: " + "https://www.rottentomatoes.com/m/" + imdbInput)
+        console.log("The movie's Rottentomatos site: " + "https://www.rottentomatoes.com/m/" + userinput);
                                                 }
             })}
+// this for loop makes it so we can launch the functions based on  index 2 of the node
+// command line. and if none of the values match a function it prompts the user to enter a correct value.
 for (var i = 2; i < nodeArgs.length; i++) {
     if (nodeArgs[2] === "movie-this") {
         getMovie();
     }
     // else if (nodeArgs[2] = "spotify-this-song") {
-    //     //spotifyfunction
+    //     getSong();
     // }
     // else if (nodeArgs[2] = "my-tweets") {
-    //     //twitterfunction
+    //     lastTweets
     // }
     // else if (nodeArgs[2] = "do-what-it-says") {
-    //     //readrandom.txt function
+    //    runRandomTxt();
     // }
      else {
     console.log("Please type: movie-this, spotify-this-song, my-tweets, or do-what-it-says");
